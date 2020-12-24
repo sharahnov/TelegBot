98fc9d3db67120de509239d454c63f4101623264
sharahnov <64405563+sharahnov@users.noreply.github.com>
2020-12-24 07:27:55 +0300

    deleting
    there were unnecessary folders

--- javaBot/src/main/java/Game.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public interface Game {
-    void next(long player, String way);
-    void setWay(long player, int way);
-    Boolean isFinished();
-    UserMap getMessage();
-    long getOpponentId(long player);
-}
