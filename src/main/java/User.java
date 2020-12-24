3be304b351da2eb05b53f7c5b22a25daeecf0fe4
sharahnov <64405563+sharahnov@users.noreply.github.com>
2020-12-24 07:35:58 +0300

    Prepared for loading from DB

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,15 +1,51 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class User {
-    Long id;
+    private Long id;
+    int messageId;
     String state;
     Game game;
-    List<int[][]> statistics;
+    private int[][] statistics;
     ArrayList<ArrayList<String>> buttons;
 
     User(long id){
         this.id = id;
-        state = "";
+        state = "default";
+        statistics = new int[2][3];
+    }
+
+    User(long id, int[][] statistics){
+        this.id = id;
+        this.statistics = statistics;
+        buttons = new ArrayList<>();
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public int[][] getStatistics(){ return statistics; }
+
+    public void  updateStatistics(Enums.statisticState state){
+        switch(state){
+            case PvPwin:
+                statistics[0][0]++;
+                break;
+            case PvEwin:
+                statistics[1][0]++;
+                break;
+            case PvPdraw:
+                statistics[0][1]++;
+                break;
+            case PvEdraw:
+                statistics[1][1]++;
+                break;
+            case PvPlose:
+                statistics[0][2]++;
+                break;
+            case PvElose:
+                statistics[1][2]++;
+                break;
+        }
     }
 }
